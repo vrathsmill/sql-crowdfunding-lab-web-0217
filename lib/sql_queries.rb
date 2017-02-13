c100ac721a3a7ae2a5dc17974061848395b9cfb4
vrathsmill <vlrathsmill@gmail.com>
2017-02-13 17:41:58 -0500

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,31 +1,31 @@
-# Write your sql queries in this file in the appropriate method like the example below:
-#
-# def select_category_from_projects
-# "SELECT category FROM projects;"
-# end
-
-# Make sure each ruby method returns a string containing a valid SQL statement.
-
 def selects_the_titles_of_all_projects_and_their_pledge_amounts
-"Write your SQL query Here"
+  "SELECT title, SUM(pledges.amount) FROM projects INNER JOIN pledges ON projects.id = pledges.project_id GROUP BY title";
 end
 
-def selects_the_user_name_age_and_pledge_amount_for_all_pledges
-"Write your SQL query Here"
-end
+
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+  "SELECT title, (SUM(pledges.amount) - projects.funding_goal) FROM projects INNER JOIN pledges ON projects.id = pledges.project_id GROUP BY title HAVING SUM(pledges.amount) >= projects.funding_goal";
 end
 
+
+def selects_the_user_name_age_and_pledge_amount_for_all_pledges
+  "SELECT name, age, SUM(pledges.amount) FROM users INNER JOIN pledges on users.id = pledges.user_id GROUP BY name"
+end
+
+
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount
-"Write your SQL query Here"
+  "SELECT name, SUM(pledges.amount) FROM users INNER JOIN
+  pledges on users.id = pledges.user_id GROUP BY name
+  ORDER BY SUM(pledges.amount)"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+  "SELECT category, pledges.amount FROM projects JOIN
+  pledges on projects.id = pledges.project_id WHERE projects.category == 'music'"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_book_category
-"Write your SQL query Here"
+  "SELECT category, SUM(pledges.amount) FROM projects JOIN pledges on projects.id =
+  pledges.project_id WHERE projects.category == 'books'"
 end
